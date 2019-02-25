278cb9777536c67aeb903c23e5842b07da03484a
rasskazshik <rasskazshik@gmail.com>
2019-02-25 17:21:31 +0300

    Init. Base design template based.

---
+++ contentTemplates/galery.php
@@ -0,0 +1,50 @@
+<!-- Page Content -->
+  <div class="container contentBackground">
+    <div class="row galeryRow">
+      <div class="col-md-8 mb-5 slider">
+        <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselIndicators" data-slide-to="2"></li>
+            </ol>
+            
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="https://static1.squarespace.com/static/5b37f80ae749408527243c80/5b3e87f3562fa74c3a31511d/5b3e8827aa4a99056da6eac1/1530825020173/macbeth-750x500.jpg" alt="Первый слайд">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="http://www.pavlov.ru/wp-content/uploads/Stella-na-vezde-v-Staryj-Izborsk-750x500.jpg" alt="Второй слайд">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="http://www.pavlov.ru/wp-content/uploads/Osen-v-Izborskoj-Kreposti-750x500.jpg" alt="Третий слайд">
+              </div>
+            </div>
+            
+            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+      </div>
+        
+      <div class="col-md-4 mb-5">
+        <h2>Список альбомов</h2>
+        <hr>
+        <ol>
+            <li>qwe qwqwe</li>
+            <li>qwe qwqwe</li>
+            <li>qwe qwqwe</li>
+            <li>qwe qwqwe</li>
+            <li>qwe qwqwe</li>
+            <li>qwe qwqwe</li>
+            <li>qwe qwqwe</li>
+        </ol>
+      </div>        
+    </div>      
+  </div>
+  <!-- /.container -->
---
+++ contentTemplates/homeContent.php
@@ -0,0 +1,109 @@
+<!-- Page Content -->
+  <div class="container contentBackground">
+
+    <div class="row">
+      <div class="col-md-8 mb-5">
+        <h2>Почему Вы выберите нашу студию</h2>
+        <hr>
+        <p>Мы предлагаем Вам провести фотосеты в прекрасной, уютной, хорошо оборудованой студии, с возможностью оформления по выбраной Вами тематике. Также производим обслуживание мероприятий и выезды на выбранные Вами объекты. Если у вас есть работа - мы готовы работать!</p>
+        <p>Если для Вас важно получить прекрасные и уникальные фотосеты - мы поможем Вам! Если для Вас важно работать в уютной и креативной студии - мы поможем Вам! Если для Вас важно работать с опытным и инициативным профессионалом - мы поможем Вам!</p>
+        <a class="btn btn-primary btn-lg" href="./calc.php">Рассчитать примерную стоимость работы</a>
+      </div>
+      <div class="col-md-4 mb-5">
+        <h2>Свяжитесь с нами</h2>
+        <hr>
+        <address>
+          <strong>Адрес</strong>
+          <br>Город Курск
+          <br>улица Заливная, дом 39
+          <br>
+        </address>
+        <address>
+          <abbr title="Phone">Телефон:</abbr>
+          <a href="tel:89191792677">+7-(919)-179-26-77</a>
+          <br>
+          <abbr title="Email">Электронная почта:</abbr>
+          <a href="mailto:#">inessaverigina@yandex.ru</a>
+        </address>
+      </div>        
+    </div>
+    <!-- /.row -->
+    <a name="galery"></a>
+    <div class="row">
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/category/svadebnoe.jpg" alt="Свадебное фото">
+          <div class="card-body">
+            <h4 class="card-title">Свадебное фото</h4>
+            <p class="card-text">Запечатлим самый запоминающийся момент вашей жизни не отвлекая вас от торжества!</p>
+          </div>
+          <div class="card-footer">
+            <a href="galery.php?category=svadba" class="btn btn-primary">Смотрим!</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/category/lovestory.jpg" alt="Лавстори">
+          <div class="card-body">
+            <h4 class="card-title">Лавстрори</h4>
+            <p class="card-text">Хотите надолго сохранить эмоции от встречи с любимым человеком? Мы поможем вам не забыть эту историю!</p>
+          </div>
+          <div class="card-footer">
+            <a href="galery.php?category=lovestory" class="btn btn-primary">Смотрим!</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/category/portret.jpg" alt="Портретное фото">
+          <div class="card-body">
+            <h4 class="card-title">Портретное фото</h4>
+            <p class="card-text">Портретное фото - это не так просто, как кажется на первый взгляд... Но мы  - профессионалы!</p>
+          </div>
+          <div class="card-footer">
+            <a href="galery.php?category=portret" class="btn btn-primary">Смотрим!</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/category/child.jpg" alt="Детское фото">
+          <div class="card-body">
+            <h4 class="card-title">Детское фото</h4>
+            <p class="card-text">Дети так быстро растут... Но фотоальбом может сохранить мгновенья с вашим все еще маленьким чудом!</p>
+          </div>
+          <div class="card-footer">
+            <a href="galery.php?category=child" class="btn btn-primary">Смотрим!</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/category/studia.jpg" alt="Студийное фото">
+          <div class="card-body">
+            <h4 class="card-title">Студийные работы</h4>
+            <p class="card-text">Винтаж, маскарад, фэнтезийный образ... Попробуйте вжиться в экзотическую роль!</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Смотрим!</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/category/outdoor.jpg" alt="Выездное фото">
+          <div class="card-body">
+            <h4 class="card-title">Выездное фото</h4>
+            <p class="card-text">Вы всегда хотели фотосет на крыше заброшенного дома? Нам не страшно - мы готовы работать где угодно!</p>
+          </div>
+          <div class="card-footer">
+            <a href="galery.php?category=outdoor" class="btn btn-primary">Смотрим!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
